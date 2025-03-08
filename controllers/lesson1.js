a6c927513d5e742aa2798d80806b0ebee1ee64ee
quelithogab <quelithogabriel28@gmail.com>
2025-03-08 15:13:02 -0500

    lesson1 added

---
+++ controllers/lesson1.js
@@ -0,0 +1,14 @@
+// const quelithoRoute = (req, res) => {
+//     res.send('Hello Quelitho Gabriel');
+  
+// }); 
+
+// const gabrielRoute = (req, res) => {
+//     res.send('Hello Gabriel');
+  
+// });  
+
+// module.exports = {
+//     quelithoRoute,
+//     gabrielRoute,
+// };
